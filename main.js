bb13bb39342db761d9b3018675cbda9476275a96
Asa-kawa <kh7672ak@gmail.com>
2024-08-16 01:01:26 +0900

    a

--- main.js
+++ main.js
@@ -2,24 +2,31 @@ document.getElementById("description").addEventListener("click", () => {
   alert("ここには何もありません");
 });
 
-document.getElementById("close").addEventListener("click", () => {
-  if (!confirm("本当に閉じていいですか？") || !confirm("本当の本当に閉じていいですか？")) {
-    alert("キャンセルしました");
-    return;
-  }
-
-  window.close();
-
-  if (!window.closed) {
-    alert("閉じるのに失敗しました");
-  }
-
-  alert(window.top === window)
-  alert(history.length)
-
-  
-});
-
 document.getElementById("index").addEventListener("click", () => {
   location.href = "https://asa-kawa.github.io/Asa-kawaIndex/";
 });
+
+function setupCloseButton() {
+  const close_button = document.getElementById("close");
+
+  if(window.opener || (window.top === window && history.length <= 1)) {
+    close_button.textContent = "終了";
+
+    close_button.addEventListener("click", () => {
+      if (!confirm("本当に閉じていいですか？") || !confirm("本当の本当に閉じていいですか？")) {
+        alert("キャンセルしました");
+        return;
+      }
+    
+      window.close();
+    });
+
+    return;
+  }
+
+  close_button.addEventListener("click", () => {
+    alert("閉じれません");
+  });
+}
+
+setupCloseButton();
