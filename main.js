3824042c44632f228171f223c1f90d42a63bbcfd
Asa-kawa <kh7672ak@gmail.com>
2024-08-17 13:59:31 +0900

    Update main.js

--- main.js
+++ main.js
@@ -5,28 +5,3 @@ document.getElementById("description").addEventListener("click", () => {
 document.getElementById("index").addEventListener("click", () => {
   location.href = "https://asa-kawa.github.io/Asa-kawaIndex/";
 });
-
-function setupCloseButton() {
-  const close_button = document.getElementById("close");
-
-  if(window.opener || (window.top === window && history.length <= 1)) {
-    close_button.textContent = "終了";
-
-    close_button.addEventListener("click", () => {
-      if (!confirm("本当に閉じていいですか？") || !confirm("本当の本当に閉じていいですか？")) {
-        alert("キャンセルしました");
-        return;
-      }
-    
-      window.close();
-    });
-
-    return;
-  }
-
-  close_button.addEventListener("click", () => {
-    alert("閉じれません");
-  });
-}
-
-setupCloseButton();
