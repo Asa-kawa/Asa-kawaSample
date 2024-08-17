5f4cff83ae83cbfb3c1be5f47d5ac1e853967de5
Asa-kawa <kh7672ak@gmail.com>
2024-08-18 01:11:32 +0900

    Update main.js

--- main.js
+++ main.js
@@ -7,5 +7,5 @@ document.getElementById("index").addEventListener("click", () => {
 });
 
 document.getElementById("???").addEventListener("click", () => {
-  location.href = "";
+  location.href = "https://github.com/Asa-kawa/Asa-kawaPassword";
 });
