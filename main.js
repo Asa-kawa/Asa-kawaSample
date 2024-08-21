9c451df5de9ce2066962df6401d72be85447e78c
Asa-kawa <kh7672ak@gmail.com>
2024-08-21 20:02:59 +0900

    変更

--- main.js
+++ main.js
@@ -2,10 +2,6 @@ document.getElementById("description").addEventListener("click", () => {
   alert("ここには何もありません");
 });
 
-document.getElementById("index").addEventListener("click", () => {
-  location.href = "https://asa-kawa.github.io/Asa-kawaIndex/";
-});
-
 document.getElementById("website-list-button").addEventListener("click", () => {
-  location.href = "https://www.youtube.com/";
+  location.href = "https://asa-kawa.github.io/Asa-kawaWebsitelist/";
 });
